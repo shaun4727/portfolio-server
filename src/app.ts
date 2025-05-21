8b7ae8bcac4f0f039b91b9a41adca78e292b89d1
Shaun Hossain <107396795+shaun4727@users.noreply.github.com>
2025-05-21 22:52:32 +0600

    updated origin

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: true,
-    // origin: ['https://portfolio-app-dashboard.vercel.app'],1
+    // origin: true,
+    origin: ['https://portfolio-app-dashboard.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
